e27fbf9a72a038ac3556da63781712b49f740c6d
GunnDawg <cgunn86@gmail.com>
2019-02-01 21:30:22 -0800

    Added library linking with #pragma comment, instead of project properties

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include "Game.h"
 #include "GameSettings.h"
 
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2_image.lib")
+#pragma comment(lib, "SDL2_ttf.lib")
+#pragma comment(lib, "SDL2_mixer.lib")
+
 int main(int argc, char* args[])
 {
 	Game* game = &Game::GetInstance();
