22b2ee59d57ec2eba1733c3ed309f580d7c62e46
GunnDawg <cgunn86@gmail.com>
2019-02-14 02:42:18 -0800

    Added instance to Game class

--- IO/Mouse.cpp
+++ IO/Mouse.cpp
@@ -1,15 +1,19 @@
 #include "Mouse.h"
+#include "Game.h"
 
 Mouse::Mouse(bool defaultState) :
 m_Enabled(defaultState),
 m_MouseX(0), m_MouseY(0)
 {
 	assert(typeid(defaultState) == typeid(bool));
+
+	game = &Game::GetInstance();
 }
 
 void Mouse::Update(Paddle* t, Paddle* r, Paddle* b, Paddle* l)
 {
-	SDL_GetMouseState(&m_MouseX, &m_MouseY);
+	m_MouseX = game->mouseX;
+	m_MouseY = game->mouseY;
 	m_MouseY -= (r->getRect().h / 2);
 	m_MouseX -= (t->getRect().w / 2);
 
